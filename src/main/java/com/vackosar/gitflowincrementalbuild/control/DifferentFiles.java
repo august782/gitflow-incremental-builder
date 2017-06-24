3fa6d12d83dacb448f94e06099c98f136877f90c
August Shi <awshi2@illinois.edu>
2017-06-24 16:31:44 -0500

    Adding in new feature for using a commit as a reference instead of a branch

--- src/main/java/com/vackosar/gitflowincrementalbuild/control/DifferentFiles.java
+++ src/main/java/com/vackosar/gitflowincrementalbuild/control/DifferentFiles.java
@@ -4,6 +4,7 @@ import com.vackosar.gitflowincrementalbuild.boundary.Configuration;
 import org.codehaus.plexus.logging.Logger;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
@@ -36,7 +37,17 @@ public class DifferentFiles {
         RevCommit base = getBranchHead(configuration.baseBranch);
         final TreeWalk treeWalk = new TreeWalk(git.getRepository());
         treeWalk.addTree(base.getTree());
-        treeWalk.addTree(resolveReference(base).getTree());
+        // Use a commit SHA to be reference if it is set; otherwise do default branch reference
+        if (!configuration.referenceCommit.equals("")) {
+            RevWalk walk = new RevWalk(git.getRepository());
+            RevCommit refCommit = walk.parseCommit(ObjectId.fromString(configuration.referenceCommit));
+            walk.close();
+            logger.info("Reference commit is: " + refCommit.getId());
+            treeWalk.addTree(refCommit.getTree());
+        }
+        else {
+            treeWalk.addTree(resolveReference(base).getTree());
+        }
         treeWalk.setFilter(TreeFilter.ANY_DIFF);
         treeWalk.setRecursive(true);
         final Path workTree = git.getRepository().getWorkTree().toPath().normalize().toAbsolutePath();
