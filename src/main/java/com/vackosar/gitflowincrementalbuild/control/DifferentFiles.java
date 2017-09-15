c94f69fbe5c051408fbaec892ab821d5696ee235
August Shi <awshi2@illinois.edu>
2017-09-15 16:25:42 -0500

    Removing unnecessary import

--- src/main/java/com/vackosar/gitflowincrementalbuild/control/DifferentFiles.java
+++ src/main/java/com/vackosar/gitflowincrementalbuild/control/DifferentFiles.java
@@ -5,7 +5,6 @@ import org.codehaus.plexus.logging.Logger;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.revwalk.filter.RevFilter;
